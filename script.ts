0bf0dda14cc3bf5eaa0642d702c259036fb52faa
DamianZajac01 <94869155+DamianZajac01@users.noreply.github.com>
2023-11-26 18:37:00 +0100

    Update script.ts

--- script.ts
+++ script.ts
@@ -20,9 +20,9 @@ const noStyle: string = "";
 const firstStyle: string = "styles/style1/styles.css";
 const secondStyle: string = "styles/style2/styles.css";
 const styles: string[] = [noStyle, firstStyle, secondStyle];
+
 function generateCssLinks(styles: string[]): void {
     const header: HTMLElement | null = document.getElementsByTagName("header")[0];
-    console.log(header);
     const title: HTMLHeadingElement = document.createElement("h1");
     const titleText: Text = document.createTextNode("Możliwe style do wybrania:");
     title.appendChild(titleText);
